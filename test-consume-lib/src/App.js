f842e59d53a361f2c315ea60286286f5ea743d9c
Justin Cole <justin.cole@digitas.com>
2023-06-08 14:08:13 +1200

    initial commit

---
+++ test-consume-lib/src/App.js
@@ -0,0 +1,12 @@
+import { AtButton } from 'vite-component-library-template'
+import 'vite-component-library-template/dist/style.css'
+
+function App() {
+  return (
+    <div className="App">
+      <AtButton label="Button" onClick={() => {}} variant="PRIMARY" />
+    </div>
+  )
+}
+
+export default App
